dddb48ab52b049b836233eeecd33f39ed3741edb
Andre Aru <aa.andrearu@gmail.com>
2018-04-18 09:31:26 +0300

    klasside hierarhia

---
+++ tarkvaraarendus/03 - klasside hierarhia.py
@@ -0,0 +1,24 @@
+class mööbel:
+    def joonista(self):
+        print(f'joonistan')
+    pikkus = ''
+    kõrgus = ''
+    laius = ''
+    värv = ''
+    materjal = ''
+
+laud = mööbel()
+laud.pikkus = '100'
+laud.kõrgus = '60'
+laud.laius = '30'
+laud.värv = 'pruun'
+laud.materjal = 'puit'
+
+tool = mööbel()
+kapp = mööbel()
+voodi = mööbel()
+diivan = mööbel()
+taburett = mööbel()
+tumba = mööbel()
+baaritool = mööbel()
+välivoodi = mööbel()
